1e41ea421c620ece9da277c721ce0839b5e44327
longhao <longhaohe@gmail.com>
2016-06-03 16:45:21 +0800

    Update to class module

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-/*!
+/**
  * Come from Vue.js v1.0.24
  *
  *   https://github.com/vuejs/vue
@@ -7,31 +7,22 @@
 
 import Cache from './cache.js'
 
-const expressionCache = new Cache(1000)
+let $cache
 
-let scopeKeyword = '$'
-let globalKeyword = '_'
-
-let allowedKeywords =
-  'Math,Date,this,true,false,null,undefined,Infinity,NaN,'
-  + 'isNaN,isFinite,decodeURI,decodeURIComponent,encodeURI,'
-  + 'encodeURIComponent,parseInt,parseFloat'
-let allowedKeywordsRE =
-  new RegExp(
-      '^('
-    + (globalKeyword + ',' + allowedKeywords).replace(/,/g, '\\b|')
-    + '\\b)'
-  )
+let allowedKeywordsRE
 
 // keywords that don't make sense inside expressions
-const improperKeywords =
-    'break,case,class,catch,const,continue,debugger,default,'
-  + 'delete,do,else,export,extends,finally,for,function,if,'
-  + 'import,in,instanceof,let,return,super,switch,throw,try,'
-  + 'var,while,with,yield,enum,await,implements,package,'
-  + 'protected,static,interface,private,public'
 const improperKeywordsRE =
-  new RegExp('^(' + improperKeywords.replace(/,/g, '\\b|') + '\\b)')
+  new RegExp(
+    '^('
+    + ('break,case,class,catch,const,continue,debugger,default,'
+      + 'delete,do,else,export,extends,finally,for,function,if,'
+      + 'import,in,instanceof,let,return,super,switch,throw,try,'
+      + 'var,while,with,yield,enum,await,implements,package,'
+      + 'protected,static,interface,private,public'
+    ).replace(/,/g, '\\b|')
+    + '\\b)'
+  )
 
 const wsRE = /\s/g
 const newlineRE = /\n/g
@@ -77,26 +68,6 @@ function save (str, isString) {
 }
 
 /**
- * Path rewrite replacer
- *
- * @param {String} raw
- * @return {String}
- */
-
-function rewrite (raw) {
-  var c = raw.charAt(0)
-  var path = raw.slice(1)
-  if (allowedKeywordsRE.test(path)) {
-    return raw
-  } else {
-    path = path.indexOf('"') > -1
-      ? path.replace(restoreRE, restore)
-      : path
-    return c + scopeKeyword + '.' + path
-  }
-}
-
-/**
  * Restore replacer
  *
  * @param {String} str
@@ -109,115 +80,147 @@ function restore (str, i) {
 }
 
 /**
- * Rewrite an expression, prefixing all path accessors with
- * `scope.` and generate getter/setter functions.
- *
- * @param {String} exp
- * @return {Function}
- */
-
-function compileGetter (exp) {
-  if (improperKeywordsRE.test(exp)) {
-    if (process.env.NODE_ENV !== 'production' && console && console.warn) {
-      console.warn('Avoid using reserved keywords in expression: ' + exp)
-    }
-  }
-  // reset state
-  saved.length = 0
-  // save strings and object literal keys
-  var body = exp
-    .replace(saveRE, save)
-    .replace(wsRE, '')
-  // rewrite all paths
-  // pad 1 space here becaue the regex matches 1 extra char
-  body = (' ' + body)
-    .replace(identRE, rewrite)
-    .replace(restoreRE, restore)
-  return makeGetterFn(body)
-}
-
-/**
- * Build a getter function. Requires eval.
- *
- * We isolate the try/catch so it doesn't affect the
- * optimization of the parse function when it is not called.
- *
- * @param {String} body
- * @return {Function|undefined}
- */
-
-function makeGetterFn (body) {
-  try {
-    /* eslint-disable no-new-func */
-    return new Function(
-      scopeKeyword,
-      globalKeyword,
-      'return ' + body + ';'
-    )
-    /* eslint-enable no-new-func */
-  } catch (e) {
-    if (process.env.NODE_ENV !== 'production' && console && console.warn) {
-      console.warn(
-          'Invalid expression. '
-        + 'Generated function body: ' + body
-      )
-    }
-  }
-}
-
-/**
  * Check if an expression is a simple path.
  *
- * @param {String} exp
+ * @param {String} expr
  * @return {Boolean}
  */
 
-function isSimplePath (exp) {
-  return pathTestRE.test(exp)
+function isSimplePath (expr) {
+  return pathTestRE.test(expr)
     // don't treat true/false as paths
-    && !booleanLiteralRE.test(exp)// &&
+    && !booleanLiteralRE.test(expr)// &&
     // Math constants e.g. Math.PI, Math.E etc.
-    // && exp.slice(0, 5) !== 'Math.'
+    // && expr.slice(0, 5) !== 'Math.'
 }
 
 /**
- * Set global keyword
- *
- * @param {String} key
+ * @param {Object} config
+ *   - {Number} cache, default 1000
+ *              limited for Cache
+ *   - {String} global, default "_"
+ *              for global string in parsed expression,
+ * @constructor
  */
 
-function setGlobal (key) {
-  if (!key) return
-  globalKeyword = key
-  allowedKeywordsRE =
-    new RegExp(
-        '^('
-      + (globalKeyword + ',' + allowedKeywords).replace(/,/g, '\\b|')
-      + '\\b)'
-    )
-}
+export default class Gep {
+  constructor ({
+    cache = 1000,
+    global = '_',
+  } = {}) {
+    this.global = global
+    this.scope = '$'
 
-/**
- * Parse an expression into a function.
- *
- * @param {String} exp
- * @return {Function}
- */
-
-export default function parseExpression (exp) {
-  exp = exp.trim()
-  // try cache
-  var hit = expressionCache.get(exp)
-  if (hit) {
-    return hit
+    $cache = new Cache(cache)
+    let allowedKeywords = global + ','
+      + 'Math,Date,this,true,false,null,undefined,Infinity,NaN,'
+      + 'isNaN,isFinite,decodeURI,decodeURIComponent,encodeURI,'
+      + 'encodeURIComponent,parseInt,parseFloat'
+    allowedKeywordsRE =
+      new RegExp(
+          '^('
+        + (allowedKeywords).replace(/,/g, '\\b|')
+        + '\\b)'
+      )
   }
-  var res = isSimplePath(exp) && exp.indexOf('[') < 0
-    // optimized super simple getter
-    ? makeGetterFn(scopeKeyword + '.' + exp)
-    // dynamic getter
-    : compileGetter(exp)
-  expressionCache.put(exp, res)
-  return res
-}
 
-parseExpression.setGlobal = setGlobal
+  /**
+   * Rewrite an expression, prefixing all path accessors with
+   * `scope.` and return the new expression.
+   *
+   * @param {String} expr
+   * @return {String}
+   */
+
+  compile (expr) {
+    if (improperKeywordsRE.test(expr)) {
+      if (process.env.NODE_ENV !== 'production'
+        && console && console.warn
+      ) {
+        console.warn('Avoid using reserved keywords in expression: ' + expr)
+      }
+    }
+    // reset state
+    saved.length = 0
+    // save strings and object literal keys
+    let body = expr
+      .replace(saveRE, save)
+      .replace(wsRE, '')
+    // rewrite all paths
+    // pad 1 space here becaue the regex matches 1 extra char
+    body = (' ' + body)
+      .replace(identRE, (raw) => {
+        let c = raw.charAt(0)
+        let path = raw.slice(1)
+        if (allowedKeywordsRE.test(path)) {
+          return raw
+        } else {
+          path = path.indexOf('"') > -1
+            ? path.replace(restoreRE, restore)
+            : path
+          return c + this.scope + '.' + path
+        }
+      })
+      .replace(restoreRE, restore)
+    return body
+  }
+
+  /**
+   * Build a getter function. Requires eval.
+   *
+   * We isolate the try/catch so it doesn't affect the
+   * optimization of the parse function when it is not called.
+   *
+   * @param {String} body
+   * @return {Function|undefined}
+   */
+
+  make (body) {
+    try {
+      /* eslint-disable no-new-func */
+      return new Function(
+        this.scope,
+        this.global,
+        'return ' + body + ';'
+      )
+      /* eslint-enable no-new-func */
+    } catch (e) {
+      if (process.env.NODE_ENV !== 'production'
+        && console && console.warn
+      ) {
+        console.warn(
+            'Invalid expression. '
+          + 'Generated function body: ' + body
+        )
+      }
+    }
+  }
+
+  /**
+   * Parse an expression.
+   *
+   * @param {String} expr
+   * @param {Boolean} toFunc
+   *                  make the parsed expression if true
+   * @return {Function}
+   */
+
+  parse (expr, toFunc) {
+    expr = expr.trim()
+    // try cache
+    var hit = $cache.get(expr)
+    if (hit) {
+      return hit
+    }
+    var res = isSimplePath(expr) && expr.indexOf('[') < 0
+      // optimized super simple getter
+      ? this.scope + '.' + expr
+      // dynamic getter
+      : this.compile(expr)
+    $cache.put(expr, res)
+    if (toFunc) {
+      res = this.make(res)
+    }
+    return res
+  }
+}
