911b5da9ab3bb90f1c77d6d026193ae4282eb9b0
longhao <longhaohe@gmail.com>
2016-06-03 17:35:35 +0800

    Update test

--- test/test.js
+++ test/test.js
@@ -1,46 +1,31 @@
 var assert = require('chai').assert
-var gep = require('../dist/gep.js')
+var Gep = require('../dist/gep.js')
+var gep = new Gep()
 
 var equal = assert.equal
 
 /* global describe, it */
 describe('gep', function () {
   var scope = {
-    a: 1,
+    radius: 3,
+    unit: 'm²',
+  }
+  var global = {
     square: function (n) {
       return n * n
     },
+    fixed: function (numObj, num) {
+      return numObj.toFixed(num)
+    },
   }
-  var global = {
-    a: 1,
-  }
-  it('"a + _.a" should equal 2', function () {
-    var expr = 'a + _.a'
-    var res = gep(expr)(scope, global)
-    equal(res, 2)
-  })
-  it('"Math.PI * square(a)" should equal Math.PI', function () {
-    var expr = 'Math.PI * square(a)'
-    var res = gep(expr)(scope)
-    equal(res, Math.PI)
-  })
-  it('"(a - _.a) === 0 ? true : false" should equal true', function () {
-    var expr = '(a - _.a) === 0 ? true : false'
-    var res = gep(expr)(scope, global)
+  it('"(_.square(radius) + unit) === \'9m²\' ? true : false" should equal "12.14m²"', function () {
+    var expr = '(_.square(radius) + unit) === \'9m²\' ? true : false'
+    var res = gep.parse(expr, true)(scope, global)
     equal(res, true)
   })
-  it('"a - global.a + Math.PI" should equal 2', function () {
-    gep.setGlobal('global')
-    var expr = 'a - global.a + Math.PI'
-    var res = gep(expr)(scope, global)
-    equal(res, Math.PI)
-  })
-  it('"++global.a" should equal 2', function () {
-    var expr = '++global.a'
-    var res = gep(expr)(scope, global)
-    equal(res, 2)
-  })
-  it('now global.a should equal 2', function () {
-    equal(global.a, 2)
+  it('"_.fixed(Math.PI + _.square(radius), 2) + unit" should equal "12.14m²"', function () {
+    var expr = '_.fixed(Math.PI + _.square(radius), 2) + unit'
+    var res = gep.parse(expr, true)(scope, global)
+    equal(res, '12.14m²')
   })
 })
